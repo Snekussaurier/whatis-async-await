eb91e3efe02d70cc8334a21580258519c5f2213c
Snekussaurier <valentin.schiffl@gmail.com>
2024-09-25 21:16:00 +0200

    Init

---
+++ AsyncAwaitCodeExample/AsyncAwaitHttp/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// See https://aka.ms/new-console-template for more information
+
+namespace AsyncAwaitHttp;
+
+internal static class Program
+{
+    static async Task Main(string[] args)
+    {
+        var client = new HttpClient();
+        Console.WriteLine("Get Google!");
+        var task1 = client.GetStringAsync("https://google.com");
+        
+        Console.WriteLine(await task1);
+        Console.WriteLine("Get Bing!");
+        var task2 = client.GetStringAsync("https://bing.com");
+    
+        Console.WriteLine(await task2);
+        Console.WriteLine("Done!");
+    }
+}
---
+++ AsyncAwaitCodeExample/AsyncAwaitTea/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace AsyncAwaitTea;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        BrewWater();
+        CleanCup();
+        AddTeaToCup();
+        AddWaterToCup();
+    }
+
+    private static void BrewWater()
+    {
+        Console.WriteLine("Beginne das Wasser zu kochen");
+        Task.Delay(2000).Wait();
+        Console.WriteLine("Wasser ist fertig");
+    }
+
+    private static void CleanCup()
+    {
+        Console.WriteLine("Reinige die Tasse");
+        Task.Delay(1000).Wait();
+    }
+
+    private static void AddTeaToCup()
+    {
+        Console.WriteLine("Füge Tee zur Tasse hinzu");
+        Task.Delay(1000).Wait();
+    }
+
+    private static void AddWaterToCup()
+    {
+        Console.WriteLine("Füge Wasser zur Tasse hinzu");
+        Task.Delay(1000).Wait();
+    }
+}
